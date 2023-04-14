e5c68e8204f92ac62edd0f592a465785e0c8da9b
Hadson Roque <nineonezero.light@gmail.com>
2023-04-14 15:23:49 +0200

    Create class indirizzi

---
+++ csharp-lista-indirizzi/Indirizzi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    internal class Indirizzi
+    {
+        //Name,Surname,Street,City,Province,ZIP
+        public string? Nome { get; }
+        public string? Cognome { get; }
+        public string Via { get; set; }
+        public string Citta { get; set; }
+        public string Provincia { get; set; }
+
+        //Costruttore della classe
+        public Indirizzi(string? nome, string? cognome, string via, string citta, string provincia)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            Via = via;
+            Citta = citta;
+            Provincia = provincia;
+        }
+    }
+}
